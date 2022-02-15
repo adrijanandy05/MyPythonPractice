9bf235c698e7eb9d813e8bd40aa7a883d3530379
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-02-15 22:48:03 +0530

    While loop and break and continue
    Also contains break and continue statements use

---
+++ seventhProg.py
@@ -0,0 +1,33 @@
+"""
+i=0
+while(True):
+    if i+1<5:
+        i=i+1
+        continue
+    print(i)
+    if i==44:
+        break
+    i=i+1
+print(i)
+
+"""
+while(True):
+    var=int(input())
+    if var>100:
+        print("Won")
+        break
+    else:
+        continue
+print("Hello")
+
+
+i=0
+while(True):
+   if i+1>45:
+       break
+   else:
+       i=i+1
+       print(i)
+       continue
+
+
