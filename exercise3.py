af8b3afdbcd5d493e1263ee3f07e823f93518d76
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-02-16 00:30:28 +0530

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,9 +1,14 @@
 correct_num=18
-while(True):
+i=0
+while(i+1<=9):
     var=int(input("Guess the no."))
     if var==correct_num:
         print("You won")
         break
+    elif var<correct_num:
+        print("Guess a greater no.")
     else:
-        print("Keep guessing")
-        continue
+        print("Guess a lesser no.")
+    i=i+1
+if i+1>9:
+    print("No. of guesses over")
