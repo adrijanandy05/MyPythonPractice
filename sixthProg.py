21dbc172a8233c57106ec56ec1478be1249a6fd0
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-02-15 22:46:57 +0530

    For loop

---
+++ sixthProg.py
@@ -0,0 +1,23 @@
+#list1=["Hello","How","Are"]
+#print(list1)
+
+list1=[["Harry",1],["Larry",2],["Carie",3],["Marie",4]]
+for item in list1:
+    print(item)
+for item,l1 in list1:
+    print(item,l1)
+dict1=dict(list1)
+print(dict1)
+for item in dict1:
+    print(item)
+for item,lollypop in dict1.items():
+    print(item,lollypop)
+
+
+
+list2=["Harry",76,"Pray",89,5]
+for item in list2:
+    if str(item).isnumeric() and item>6:
+        print(item)
+    else:
+        print("Invalid")
