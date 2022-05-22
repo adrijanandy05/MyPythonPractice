bd92d386563751045c09949fcb075617d7cb6a19
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-05-22 13:02:51 +0530

    Scope (Global variable and global keyword)

---
+++ tenthProg.py
@@ -0,0 +1,35 @@
+"""
+l=10  #global ariable
+def func1(n):
+    l=5  #local variable
+    print(l)  #will print the local variable if defined any otherwise will print the global variable
+    print(n,"I have printed")
+print(l)  #will print the global variable
+func1("Hello")
+print(l)  #even after calling the function once before, l takes the value of the global variable
+"""
+x=90
+def harry():
+    x=20
+    def rohan():
+        global x
+        x=88
+        print(x)
+    rohan()
+    print(x)
+harry()
+print(x)
+
+"""
+x=90
+def harry():
+	x=20
+	def rohan():
+		global x
+		x=88
+	print(x)    ------> takes the value of local variable of harry() so prints 20
+	rohan()   
+	print(x)   --------> takes the value of local variable of harry() so prints 20
+harry()  ------> 20 20
+print(x) -------> will change the x value to the global one so prints 88 even though its defined outside as 90 but since "global" been used inside the function so will change the value to 88
+"""
