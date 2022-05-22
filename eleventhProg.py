6068d4f60b2794bb918ca1f751ab61914492daa7
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-05-22 22:48:14 +0530

    Factorial and Fibonacci using functions

---
+++ eleventhProg.py
@@ -0,0 +1,18 @@
+def fact(n):
+    fac=1
+    for i in range(n):
+        fac=fac*(i+1)
+    return fac
+
+num=int(input("Enter a number: "))
+print("Factorial of the number is ",fact(num))
+
+def fib(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-1)
+num1=int(input("Enter a number: "))
+print("Fibonacci number is ",fib(num1))
