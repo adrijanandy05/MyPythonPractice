0b15ff19fd304f4203d0191a2a0068e0cba910de
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-05-22 11:21:55 +0530

    Dictionary basics

---
+++ fourthProg.py
@@ -0,0 +1,10 @@
+d1={"Harry":"Magic","June":{"L":"Chicken","B":"Bread","D":"Chocolate"},"Ria":"Books"}
+print(d1["June"])
+print(d1["June"]["B"])
+d1.update({"Tom":"Spiderman"})
+print(d1)
+print(d1.get("Harry"))
+print(d1.keys())
+print(d1.items())
+d1["Tony"]="Iron man"
+print(d1)
