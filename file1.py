12024e880c142e071517456250ffd50f376ef8cd
Adrija Nandy <36583713+adrijanandy05@users.noreply.github.com>
2022-05-29 00:19:42 +0530

    use of main
    if __name__=='__main__'

---
+++ file1.py
@@ -0,0 +1,4 @@
+import file2
+
+#executes everythin of file2 before main but nothing which is inside main
+print(file2.add(10,20))
---
+++ file2.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b+100
+
+print(add(5,3))
+
+#using main
+if __name__ == '__main__':
+    print(add(40,50))
